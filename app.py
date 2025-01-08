62ad19826a48f56a4b54b39d8a08b977d5d66fd6
Adam <aplochocki124@gmail.com>
2025-01-08 21:17:38 +0100

    final commit (pls). switched from whisper to the speechrecognition package

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import os
-import whisper
+from pydub import AudioSegment
+import speech_recognition as sr
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-model = whisper.load_model("tiny.en")
+r = sr.Recognizer()
 
 @app.route("/")
 def main():
@@ -24,12 +25,25 @@ def transcribe():
   file_path = os.path.join(app.config['UPLOAD_FOLDER'], audio_file.filename)
   audio_file.save(file_path)
 
-  result = model.transcribe(file_path, language="en")
-  transcription = result["text"]
+  wav_path = os.path.join(app.config['UPLOAD_FOLDER'], "audio.wav")
+  audio = AudioSegment.from_mp3(file_path)
+  audio.export(wav_path, format="wav")
 
   os.remove(file_path)
 
-  return jsonify({'transcription': transcription})
+  with sr.AudioFile(wav_path) as source:
+    sound = r.record(source)
+    result = "idk what happend but its not handled so gl"
 
+    try:
+      result = r.recognize_google(sound)
+    except sr.UnknownValueError:
+      result = "Google Speech Recognition could not understand audio"
+    except sr.RequestError as e:
+      result = "Could not request results from Google Speech Recognition service"
+    finally:
+      os.remove(wav_path)
+      return jsonify({'transcription': result})
+  
 if __name__ == '__main__':
   app.run()
